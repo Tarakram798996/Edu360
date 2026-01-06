ebe0bf824eb5e19af945fd40c044b09ce1006d1f
Tarakram798996 <jtarakram2@gmail.com>
2026-01-06 21:30:27 +0530

    Add admin features, update controllers and dashboards

--- Edu360/src/main/java/com/edu360/Edu360/Edu360Application.java
+++ Edu360/src/main/java/com/edu360/Edu360/Edu360Application.java
@@ -2,8 +2,12 @@ package com.edu360.Edu360;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.edu360.Edu360.model")
+@EnableJpaRepositories("com.edu360.Edu360.repos")
 public class Edu360Application {
 
 	public static void main(String[] args) {
